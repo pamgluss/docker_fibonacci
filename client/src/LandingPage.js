f1ab41eab741eab63d230a19ec15b720c6ca82fd
Pamela Gluss <pgluss@slack-corp.com>
2022-02-16 19:05:48 -0800

    remove debug stuff

--- client/src/LandingPage.js
+++ client/src/LandingPage.js
@@ -5,7 +5,6 @@ import { Link } from 'react-router-dom';
 const LandingPage = (props) => (
         <div>
             <p>Welcome to Pam's Fibonacci Calculator App!</p>
-            <p>Version 2</p>
             <p>This was built while
                 <a href="https://www.udemy.com/share/101WjM3@E0aQGu1Dp94xK2q0OQ3CaGW-Am2T41nSCoOAUlS42QP0X6x229hxIjycvvMnZ_t6/"> learning Docker </a>
             </p>
